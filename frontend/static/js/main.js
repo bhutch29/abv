20217c05706c197dc83f4f5fb2be77f851a54ccd
bhutch <inukage0029@gmail.com>
2018-11-27 22:44:02 -0800

    Did some organization and prettifying of the menu, still needs more work

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,20 +1,23 @@
 let persist = {};
 persist.index = 0;
 
+let timer = 5000;
+let beersPerPage = 16;
+
 function changePage(){
     $.getJSON("http://localhost:8081/inventory", function(beers){
         $("#beer-list").empty()
         if (persist.index >= beers.length) {
             persist.index = 0;
         }
-        for (var i = persist.index; i < persist.index + 2; i++) {
+        for (var i = persist.index; i < persist.index + beersPerPage; i++) {
             if (i == beers.length) {
                 break;
             }
             var html = Mustache.to_html($('#beer-entry').html(), beers[i]);
             $('<div class="grid-item"/>').html(html).appendTo('#beer-list')
         }
-        persist.index += 2;
+        persist.index += beersPerPage;
     });
 };
 
@@ -22,4 +25,4 @@ $(document).ready(changePage);
 
 window.setInterval(function(){
     changePage();
-}, 5000);
+}, timer);
